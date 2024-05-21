d44779aeeae4552cd72077a2f0d6164d250cd9d3
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-22 02:29:11 +0700

    use setup function

--- job-vacancy/src/App.vue
+++ job-vacancy/src/App.vue
@@ -2,32 +2,32 @@
   <div class="app">
     <p>{{ name }} - {{ age }}</p>
     <button @click="changeName('bill')">change name</button>
-    <button @click="changeAge('34')">change name</button>
+    <button @click="changeAge('34')">change age</button>
   </div>
 </template>
 
 <script lang="ts">
-
-import { defineComponent } from "vue";
+import { defineComponent, reactive, toRefs } from "vue";
 
 export default defineComponent({
   name: "App",
   components: {},
-  data() {
-    return {
-      name: 'Link',
-      age: 29 as number | string
-    };
+  setup() {
+    const state = reactive({
+      name: "Link",
+      age: 28 as string | number,
+    });
+    return { ...toRefs(state) };
   },
   methods: {
     changeName(name: string) {
       this.name = name;
-      return name
+      return name;
     },
     changeAge(age: string | number) {
-      this.age = age
-      return age
-    }
+      this.age = age;
+      return age;
+    },
   },
 });
 </script>
