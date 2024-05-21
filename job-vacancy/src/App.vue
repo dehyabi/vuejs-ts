b65939a48d90e30d44111fdddc06065b9c6d563e
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-22 03:38:54 +0700

    types and props

--- job-vacancy/src/App.vue
+++ job-vacancy/src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="app"></div>
+  <div class="app">
+    <JobList :jobs="jobs" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import Job from "./types/Job";
+import JobList from "./components/JobsList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       { title: "Laravel Developer", location: "USA", salary: 4000, id: "1" },
-      { title: "javascript developer", location: "Japan", salary: 4200, id: "2" },
+      {
+        title: "Javascript Developer",
+        location: "Japan",
+        salary: 4200,
+        id: "2",
+      },
       { title: "Backend developer", location: "UK", salary: 5000, id: "3" },
     ]);
-    return {jobs}
+    return { jobs };
   },
 });
 </script>
---
+++ job-vacancy/src/components/JobsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h1>{{ job.title }} in {{ job.location }}</h1>
+        <div class="salary">
+            <p>USD {{ job.salary }}</p>
+        </div>
+        <div class="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eos adipisci asperiores explicabo similique non magni! Debitis iure error ducimus fuga! Consectetur ipsum molestias eveniet deserunt sint! Harum, vel nemo.
+        </div>
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "../types/Job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+  },
+});
+</script>
+
+<style></style>
